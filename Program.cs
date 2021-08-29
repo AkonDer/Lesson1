2b4296e6b4ccb87134001c6ca9850015acc2df48
AkonDR <vjacheslav.leb@gmail.com>
2021-08-29 16:58:37 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
